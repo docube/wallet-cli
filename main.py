8970506e7e2ee1954d937bfae3524cf7b2871773
docube <segundavids091@gmail.com>
2024-08-06 15:44:54 +0100

    Update

--- main.py
+++ main.py
@@ -7,9 +7,13 @@ def main_menu():
     active = True
     while active:
         print("\nPlease select an option:")
-        print("1. Sign up")
+        print("1. Sign Up")
         print("2. Login")
-        print("3. Exit")
+        print("3. Deposit")
+        print("4. Send Money")
+        print("5. View Transactions")
+        print("6. View Single Transaction")
+        print("7. Exit")
 
         choice = input("Enter your choice (1/2/3): ")
 
@@ -17,7 +21,9 @@ def main_menu():
             paths[1]()
         elif choice == '2':
             active_user = paths[2]()
-        elif choice == '3':
+        elif choice == '3' or '4' or '5' or '6':
+            print('Please Login or Signup if you are a first time User:')
+        elif choice == '7':
             print("Exiting the program.")
             active = False
         else:
