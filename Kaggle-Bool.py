815d3b525773903140b80f04ad3a2ec4a0020e7b
docube <segundavids091@gmail.com>
2025-01-07 13:58:29 +0100

    Up

--- Kaggle-Bool.py
+++ Kaggle-Bool.py
@@ -8,3 +8,24 @@ def can_run_for_president(age):
     return age >= 35;
 print("Can a 19-year-old run for president?", can_run_for_president(19));
 print("Can a 45-year-old run for president?", can_run_for_president(45));
+
+print(3.0 == 3);
+print(3.0 == '3');
+
+def is_odd(n):
+    return (n % 2) == 1;
+
+print("Is 100 Odd?", is_odd(100));
+print("Is -17 Odd?", is_odd(-17));
+
+def can_run_for_nigerian_president(age, is_natural_born_citizen):
+    """Can someone of the given age and citizenship status run for the president of Nigeria?"""
+    # The Nigerian constitution states that you must be a natural born citizen *and* at least 35 years old
+    return is_natural_born_citizen and (age >= 35);
+
+print(can_run_for_nigerian_president(19, True));
+print(can_run_for_nigerian_president(55, False));
+print(can_run_for_nigerian_president(55, True));
+
+print(True or True and False);
+print(True and False);
