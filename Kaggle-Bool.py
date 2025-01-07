fd9dfc7fa5996d4b0ea4dc2ed91e03a88c07decd
docube <segundavids091@gmail.com>
2025-01-07 10:45:33 +0100

    Up

--- Kaggle-Bool.py
+++ Kaggle-Bool.py
@@ -1,3 +1,10 @@
 x = True;
 print(x);
 print(type(x));
+
+def can_run_for_president(age):
+    """Can someone of the given age run for the president in Nigeria?"""
+    # The Nigerian Constitution says you must be at least 35years old
+    return age >= 35;
+print("Can a 19-year-old run for president?", can_run_for_president(19));
+print("Can a 45-year-old run for president?", can_run_for_president(45));
